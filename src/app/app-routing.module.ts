0f9e2e00ec9e18daa7f505b91f7b49258efda051
YauheniZhybryk <yzhybryk@datamola.com>
2022-07-03 16:58:26 +0300

    Initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'main',
+    canActivate: [],
+    loadChildren: () => import('./main/main.module').then((m) => m.MainModule),
+  },
+  {
+    path: '**',
+    redirectTo: 'main',
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
